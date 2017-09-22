606cb8d74fe6d3752efff4019d6f2fcc24e4c72e
Johnny Graber <jg@jgraber.ch>
2017-09-22 17:31:05 +0200

    add Google Analytics back to site

--- index.php
+++ index.php
@@ -9,6 +9,15 @@
 
 -->
 <head>
+    <!-- Global Site Tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-28754106-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments)};
+      gtag('js', new Date());
+
+      gtag('config', 'UA-28754106-1');
+    </script>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
